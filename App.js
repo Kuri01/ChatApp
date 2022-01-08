2f148059d01703fcb2cd72d68d068419f1e3cbc6
Kuri01 <kamil.kuriata01@gmail.com>
2022-01-08 17:21:24 +0100

    Init project

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>hey</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
